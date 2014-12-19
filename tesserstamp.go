4267c9ccd162f1c3ecae08083fc90e6df7f88934
Arne Vansteenkiste <barnex@gmail.com>
2014-12-19 16:29:33 +0100

    timestamper

---
+++ tesserstamp.go
@@ -0,0 +1,30 @@
+/*
+timestamper. Usage:
+ 	tesseract | tesserstamp > timestamps.txt
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	start := time.Now()
+	for {
+		l, _, err := in.ReadLine()
+		if err != nil {
+			break
+		}
+		line := string(l)
+		if strings.Contains(line, "fragged") {
+			gotfragged :=  strings.Contains(line, "got")
+			stamp  := time.Since(start).Seconds()*1000
+			fmt.Println(stamp, gotfragged);
+		}
+	}
+}
